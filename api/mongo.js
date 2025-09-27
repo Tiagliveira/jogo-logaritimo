5da7a26290b56523304c3c353b2c651d8f25704a
Tiagliveira <tiagliveira92@gmail.com>
2025-09-27 00:46:34 -0300

    testando as rotas para backend

--- api/mongo.js
+++ api/mongo.js
@@ -1,18 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
 const userSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
+  avatar: String,
   nivel: { type: Number, default: 1 },
   nivelMaximo: { type: Number, default: 1 },
   vidas: { type: Number, default: 3 },
-  historico: { type: [String], default: [] },
-  avatar: String,
+  historico: { type: Array, default: [] },
 });
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+module.exports = mongoose.models.User || mongoose.model("User", userSchema, "users");
